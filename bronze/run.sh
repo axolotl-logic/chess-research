2a4e2e07b1743354ce2f374c999862ac3c1fcc0a
Autumn Green <me@axolotl-logic.io>
2025-09-28 13:02:11 -0400

    feat: pull latest month of lichess dump

--- bronze/run.sh
+++ bronze/run.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 volume_path=`dirname "$0"`/volume
-base_name=lichess_db_standard_rated_2025-07
+date_suffix=`date --date="$(date +%Y-%m-15) -1 month" +%Y-%m`
+base_name=lichess_db_standard_rated_$date_suffix
 
 remote_file="https://database.lichess.org/standard/$base_name.pgn.zst"
 input_file="$volume_path/$base_name.pgn.zst"
 output_file="$volume_path/$base_name.tsv"
+output_tmp_file="$output_file.tmp"
 
 wget -c $remote_file -O $input_file
 
-zstdcat $input_file  | pgne3k lichess.tagspec  | tqdm > $output_file
+zstdcat $input_file  | pgne3k lichess.tagspec  | tqdm > $output_tmp_file
+mv $output_tmp_file $output_file
